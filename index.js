2701030a5f6e1c9a1fb0570b48af2c3679c44b03
Yego <eddieyego273@gmail.com>
2025-03-14 15:25:49 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+   let num1= 31;
+   let num2= 2;
+   let multiply=num1*num2;
+
+   const random = Math.floor((Math.random() * 10) + 1);
+
+   let num3=32;
+   let num4=8;
+   const mod = num3/num4;
+
+let num= [5,10,15,20]
+let max=Math.max(...num)
+ 
+
+    
+    
